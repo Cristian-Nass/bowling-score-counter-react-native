214e521b6b864f8874eea12a2a3cf7f19e819ebf
Cristian Nass <cristian.tirdad@gmail.com>
2024-01-24 16:57:28 +0100

    Start the project

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const [text, onChangeText] = React.useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <Text>Please Input the name</Text>
+      <View style={stylesFlex.container}>
+        <TextInput
+          placeholder='Input the name'
+          style={styleInput.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+        <TouchableOpacity
+          style={{
+            height: 30,
+            width: 30,
+            marginTop: 18,
+            borderRadius: 4,
+            backgroundColor: "blue",
+          }}
+        >
+          <Text
+            style={{
+              color: "#fff",
+              textAlign: "center",
+              marginTop: 4,
+            }}
+          >
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +50,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+const stylesFlex = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+  },
+});
+
+const styleButton = StyleSheet.create({
+  input: {
+    color: "red",
+    height: 10,
+    width: 10,
+  },
+});
+
+const styleInput = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderRadius: 4,
+    width: 200,
+    borderWidth: 1,
+    padding: 10,
   },
 });
